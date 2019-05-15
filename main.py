7e95c8eb98feb46dcd53443728b70a7621c877ef
Daiki Gushiken <e165742@ie.u-ryukyu.ac.jp>
2019-05-15 19:45:28 +0900

    aa

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 
 app = Flask(__name__)
 
